a00016227024f7f79911480103c29d10a30a7431
AprliRainkun <zmy.2347@sjtu.edu.cn>
2018-02-25 13:33:27 +0800

    remove old files; cache protobuf

--- install_protoc.sh
+++ install_protoc.sh
@@ -8,14 +8,14 @@ error() {
     exit 1
 }
 
-if [ -n "$PROTOBUF_VERSION"]; then
-    error "PROTOBUF_VERSION is not set"
-fi
+test -n "$PROTOBUF_VERSION" || error "PROTOBUF_VERSION is not set"
+test -n "$INSTALL_PATH" || error "INSTALL_PATH is not set"
 
-if [ ! -f "/home/travis/bin/protoc"]; then
+if [ ! -d $INSTALL_PATH ] ; then 
     curl -sL https://github.com/google/protobuf/archive/v$PROTOBUF_VERSION.tar.gz | tar zx
 
     cd protobuf-$PROTOBUF_VERSION
     ./autogen.sh
-    ./configure --prefix=/home/travis && make && sudo make install
+    ./configure --prefix=$INSTALL_PATH
+    make && sudo make install
 fi
